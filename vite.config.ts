8a8f8d5d0967534348443e2c0f1a76a6875f136f
Bezliki7 <seki4calm@mail.ru>
2024-03-17 11:01:36 +0500

    🐛fix: попытка пофксить билд для gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://bezliki7.github.io/stocks/',
+  base: '/deploy-vite/',
   plugins: [react()],
 
   optimizeDeps: {
