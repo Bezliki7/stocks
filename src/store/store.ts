b8811b953fa242be18d22c9a5feefafeb82d3113
Bezliki7 <seki4calm@mail.ru>
2024-03-17 13:42:58 +0500

    🚀 stuff: обращение к серверу

--- src/store/store.ts
+++ src/store/store.ts
@@ -24,7 +24,7 @@ export class Store {
   }
 
   public getMoexIndexesByPeriod = flow(function* (this: Store) {
-    const url = 'http://localhost:3000/main';
+    const url = 'https://serevr-stocks.onrender.com/main';
 
     const res = yield axios.put(url, {
       startDate: this.startDate,
